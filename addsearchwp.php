3fa13a19136e6499350604e1c8b1f3a3070eae71
rolandinsh <rolands@e-art.lv>
2013-12-04 20:14:54 +0200

    init 1.0.0

---
+++ addsearchwp.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+  Plugin Name: AddSearch for WordPress
+  Plugin URI: http://simplemediacode.com/?s=addsearchwp
+  Description: AddSearch is an instant site search engine for your WordPress based website. 
+  Author: rolandinsh
+  Author URI: http://umbrovskis.com/
+  Version: 0.0.1
+  License: GPLv2 or later (LICENSE)
+*/
